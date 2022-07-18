4015f854a05d6722833998191ab9479b971697be
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2022-07-18 12:36:13 +0530

    Update Add_Booking1.Designer.cs

--- Add_Booking1.Designer.cs
+++ Add_Booking1.Designer.cs
@@ -19,3 +19,27 @@ namespace hotel_managemant_system
             }
             base.Dispose(disposing);
         }
+
+
+
+    
+       
+
+        private MetroFramework.Controls.MetroButton btn_sh_search;
+        private MetroFramework.Controls.MetroTextBox txt_sh_hotel_id_name;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroPanel metroPanel1;
+        private MetroFramework.Controls.MetroTextBox txt_sh_hotel_id;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTextBox txt_sh_about;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox txt_sh_address;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroTextBox txt_sh_city;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private MetroFramework.Controls.MetroTextBox txt_sh_hotel_name;
+        private MetroFramework.Controls.MetroLabel lable_ah1_hotel_name;
+        private MetroFramework.Controls.MetroButton btn_reset;
+        private MetroFramework.Controls.MetroButton btn_search_hotel_exit;
+    }
+}
