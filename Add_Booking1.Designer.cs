ec5e25401ff7a8bc757f80986ecafefb3dae3354
Akshitha Sriyanjith <masriyanjith@students.nsbm.ac.lk>
2022-07-18 12:34:38 +0530

    Create Add_Booking1.Designer.cs

---
+++ Add_Booking1.Designer.cs
@@ -0,0 +1,21 @@
+namespace hotel_managemant_system
+{
+    partial class Add_Booking1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
